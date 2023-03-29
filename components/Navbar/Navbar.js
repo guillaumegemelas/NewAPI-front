fedadb2c001e59d484d01865c1ed51282969d214
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-03-29 19:11:25 +0200

    first commit

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container-fluid justify-content-center">
+        <p className="navbar-brand mx-4">test</p>
+        <p className="navbar-brand mx-4">test1</p>
+        <p className="navbar-brand mx-4">test2</p>
+      </div>
+    </nav>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { useEffect } from "react";
+
+//import de Bootstrap de node modules
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+//import du composant Container
+import Container from "@/components/Container/Container";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  return (
+    <Container>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
