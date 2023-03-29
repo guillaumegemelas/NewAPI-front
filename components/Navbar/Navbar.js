3e5a0c03baf6195b35dad0fa1d3f7b456e598cd8
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-03-29 20:00:00 +0200

    commit ok

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-primary">
       <div className="container-fluid justify-content-center">
-        <p className="navbar-brand mx-4">test</p>
+        <Link legacyBehavior href="/">
+          <a className="navbar-brand mx-4">A la une</a>
+        </Link>
         <p className="navbar-brand mx-4">test1</p>
         <p className="navbar-brand mx-4">test2</p>
       </div>
