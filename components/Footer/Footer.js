7fd775027ceadf7a9a705238c963486eca81c842
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-03-30 21:52:39 +0200

    change url to Northflanck ok

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBrandsGithub } from "@fortawesome/free-solid-svg-icons";
+// import { faBrandsGithub } from "@fortawesome/free-solid-svg-icons"-;
 
 export default function Footer() {
   return (
