473424128307c97767b32adbfcbfd9726bfe30c6
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-05-18 08:33:13 +0200

    test

--- pages/sante.js
+++ pages/sante.js
@@ -50,7 +50,7 @@ export async function getStaticProps() {
 
   const data = await result.json();
 
-  //data renvoie bien un objet avec un tableau d'articles
+  //data renvoie bien un objet avec un tableau d'articles!
   console.log(data);
 
   return { props: { data } };
